61da3c9289e1c31f15cfb2ab4d7ffa5c497bbe21
YellowSar <hsgopoke@gmail.com>
2023-03-29 17:18:09 +0300

    debug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,68 +9,66 @@ import { addPizzaToCart } from '../redux/actions/cart';
 
 const categoryNames = ['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
 const sortIems = [
-  { name: 'популярности', type: 'popular', order: 'desc' },
-  { name: 'цене', type: 'price', order: 'desc' },
-  { name: 'алфавит', type: 'name', order: 'asc' },
+    { name: 'популярности', type: 'popular', order: 'desc' },
+    { name: 'цене', type: 'price', order: 'desc' },
+    { name: 'алфавит', type: 'name', order: 'asc' },
 ];
 
 function Home() {
-  const dispatch = useDispatch();
-  const items = useSelector(({ pizzas }) => pizzas.items);
-  const cartItems = useSelector(({ cart }) => cart.items);
-  const isLoaded = useSelector(({ pizzas }) => pizzas.isLoaded);
-  const { category, sortBy } = useSelector(({ filters }) => filters);
+    const dispatch = useDispatch();
+    const items = useSelector(({ pizzas }) => pizzas.items);
+    const cartItems = useSelector(({ cart }) => cart.items);
+    const isLoaded = useSelector(({ pizzas }) => pizzas.isLoaded);
+    const { category, sortBy } = useSelector(({ filters }) => filters);
 
-  React.useEffect(() => {
-    dispatch(fetchPizzas(sortBy, category));
-  }, [category, sortBy]);
+    React.useEffect(() => {
+        dispatch(fetchPizzas(sortBy, category));
+    }, [category, sortBy]);
 
-  const onSelectCategory = React.useCallback((index) => {
-    dispatch(setCategory(index));
-  }, []);
+    const onSelectCategory = React.useCallback((index) => {
+        dispatch(setCategory(index));
+    }, []);
 
-  const onSelectSortType = React.useCallback((type) => {
-    dispatch(setSortBy(type));
-  }, []);
+    const onSelectSortType = React.useCallback((type) => {
+        dispatch(setSortBy(type));
+    }, []);
 
-  const handleAddPizzaToCart = (obj) => {
-    dispatch({
-      type: 'ADD_PIZZA_CART',
-      payload: obj,
-    });
-  };
+    const handleAddPizzaToCart = (obj) => {
+        dispatch({
+            type: 'ADD_PIZZA_CART',
+            payload: obj,
+        });
+    };
 
-  return (
-    <div className="container">
-      <div className="content__top">
-        <Categories
-          activeCategory={category}
-          onClickCategory={onSelectCategory}
-          items={categoryNames}
-        />
-        <SortPopup
-          activeSortType={sortBy.type}
-          items={sortIems}
-          onClickSortType={onSelectSortType}
-        />
-      </div>
-      <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoaded
-          ? items.map((obj) => (
-              <PizzaBlock
-                onClickAddPizza={handleAddPizzaToCart}
-                key={obj.id}
-                addedCount={cartItems[obj.id] && cartItems[obj.id].items.length}
-                {...obj}
-              />
-            ))
-          : Array(12)
-              .fill(0)
-              .map((_, index) => <PizzaLoadingBlock key={index} />)}
-      </div>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="content__top">
+                <Categories
+                    activeCategory={category}
+                    onClickCategory={onSelectCategory}
+                    items={categoryNames}
+                />
+                <SortPopup
+                    activeSortType={sortBy.type}
+                    items={sortIems}
+                    onClickSortType={onSelectSortType}
+                />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+                {isLoaded ? items.map((obj) => (
+                    <PizzaBlock
+                        onClickAddPizza={handleAddPizzaToCart}
+                        key={obj.id}
+                        addedCount={cartItems[obj.id] && cartItems[obj.id].items.length}
+                        {...obj}
+                    />
+                )) : Array(12)
+                    .fill(0)
+                    .map((_, index) => <PizzaLoadingBlock key={index} />)}
+            </div>
+        </div>
+    );
 }
 
 export default Home;
